5fbf14fdfce77b2073b6a6b6b271fea51d2ed6a5
Jacqueline <65547052+jigavilanes1@users.noreply.github.com>
2023-03-08 12:15:04 -0500

    Add files via upload

---
+++ Controllers/PersonaController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Prueba2Parcial.Models;
+using Prueba2Parcial.Models.TableViewModels;
+using Prueba2Parcial.Models.ViewModels;
+
+namespace Practica2.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            List<PersonaTableViewModel> lst = null;
+
+            using (db_personaEntities db = new db_personaEntities())
+            {
+                lst = (from d in db.tbl_persona
+                       select new PersonaTableViewModel
+                       {
+                           CustomerId = d.customerId,
+                           CustomerName = d.customerName,
+                           ContactName = d.contactName,
+                           Address = d.address,
+                           City = d.city,
+                           PostalCode = (int) d.postalCode,
+                           Country = d.country
+                       }).ToList();
+            }
+            return View(lst);
+        }
+        [HttpGet]
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(PersonaViewModels model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            using (db_personaEntities db = new db_personaEntities())
+            {
+                tbl_persona oPersona = new tbl_persona();
+
+                oPersona.customerName = model.CustomerName;
+                oPersona.contactName = model.ContactName;
+                oPersona.address = model.Address;
+                oPersona.city = model.City;
+                oPersona.postalCode = model.PostalCode;
+                oPersona.country = model.Country;
+
+                db.tbl_persona.Add(oPersona);
+                db.SaveChanges();
+            }
+            return Redirect(Url.Content("~/Persona/"));
+        }
+
+        public ActionResult Editar(int id)
+        {
+            EditarPersonaViewModels model = new EditarPersonaViewModels();
+            using (db_personaEntities db = new db_personaEntities())
+            {
+                var oPersona = db.tbl_persona.Find(id);
+                model.CustomerId = oPersona.customerId;
+                model.CustomerName = oPersona.customerName;
+                model.ContactName = oPersona.contactName;
+                model.Address = oPersona.address;
+                model.City = oPersona.city;
+                model.PostalCode = (int) oPersona.postalCode;
+                model.Country = oPersona.country;
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(EditarPersonaViewModels model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            using (db_personaEntities db = new db_personaEntities())
+            {
+                var oPersona = db.tbl_persona.Find(model.CustomerId);
+                oPersona.customerName = model.CustomerName;
+                oPersona.contactName = model.ContactName;
+                oPersona.address = model.Address;
+                oPersona.city = model.City;
+                oPersona.postalCode = model.PostalCode;
+                oPersona.country = model.Country;
+                db.Entry(oPersona).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+            return Redirect(Url.Content("~/Persona/"));
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            using (db_personaEntities db = new db_personaEntities())
+            {
+                var oPersona = db.tbl_persona.Find(id);
+                db.tbl_persona.Remove(oPersona);
+                db.SaveChanges();
+            }
+            return Redirect(Url.Content("~/Persona/"));
+        }
+
+    }
+}
---
+++ Models/ViewModels/PersonaViewModels.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Prueba2Parcial.Models.ViewModels
+{
+    public class PersonaViewModels
+    {
+        [Required]
+        [Display(Name = "Ingrese el nombre del negocio: ")]
+        [StringLength(100, MinimumLength = 3)]
+        public string CustomerName { get; set; }
+
+        [Required]
+        [Display(Name = "Ingrese el nombre del contacto: ")]
+        [StringLength(100, MinimumLength = 3)]
+        public string ContactName { get; set; }
+
+        [Required]
+        [Display(Name = "Ingrese la direccion: ")]
+        [StringLength(100, MinimumLength = 3)]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Ingrese el pais: ")]
+        [StringLength(100, MinimumLength = 3)]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "Ingrese el codigo postal: ")]
+        public int PostalCode { get; set; }
+
+        [Required]
+        [Display(Name = "Ingrese la ciudad: ")]
+        public string Country { get; set; }
+    }
+
+    public class EditarPersonaViewModels
+    {
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        [Display(Name = "Ingrese el nombre del negocio: ")]
+        [StringLength(100, MinimumLength = 3)]
+        public string CustomerName { get; set; }
+
+        [Required]
+        [Display(Name = "Ingrese el nombre del contacto: ")]
+        [StringLength(100, MinimumLength = 3)]
+        public string ContactName { get; set; }
+
+        [Required]
+        [Display(Name = "Ingrese la direccion: ")]
+        [StringLength(100, MinimumLength = 3)]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Ingrese la ciudad: ")]
+        [StringLength(100, MinimumLength = 3)]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "Ingrese el codigo postal: ")]
+        public int PostalCode { get; set; }
+
+        [Required]
+        [Display(Name = "Ingrese la ciudad: ")]
+        public string Country { get; set; }
+    }
+}
